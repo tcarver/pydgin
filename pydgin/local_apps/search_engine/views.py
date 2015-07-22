133a22993e19c489684b64ed2e81f41b3bea9870
tcarver <tjc29@duffield>
2015-07-22 11:56:51 +0100

    search engine updates

--- pydgin/local_apps/search_engine/views.py
+++ pydgin/local_apps/search_engine/views.py
@@ -1,39 +1,47 @@
 from django.shortcuts import render
 from elastic.search import Search, ElasticQuery, Highlight
 from elastic.aggs import Agg, Aggs
+from elastic.elastic_settings import ElasticSettings
 
 
 def search_page(request):
     ''' Renders a page to allow searches to be constructed. '''
 
-    idx = 'genes_hg38_v0.0.1'
-    sources = ['symbol', 'synonyms', 'interaction_source', 'pathway_name']
-
     queryDict = request.GET
     if queryDict.get("query"):
         query = queryDict.get("query")
+        source_filter = ['symbol', 'synonyms', "dbxrefs.ensembl", 'interaction_source', 'pathway_name', 'id', 'journal']
+        highlight_fields = list(source_filter)
+        highlight_fields.extend(['abstract', 'title', 'authors.LastName', 'authors.ForeName', 'pmids'])
         fields = []
-        for it in queryDict.items():
-            if len(it) == 2:
-                parts = it[1].split(":")
-                if len(parts) > 1:
-                    if len(parts) > 2:
-                        fields.append(parts[1]+".*")
-                    else:
-                        fields.append(parts[1])
+        if not (isinstance(query, int) or query.isdigit()):
+            fields.extend(highlight_fields)
+        source_filter.extend(['PMID'])
+
+        idx_name = queryDict.get("idx")
+        idx_type = ''
+        if idx_name == 'ALL':
+            idx = ElasticSettings.indices_str()
+        else:
+            idx = ElasticSettings.attrs().get('IDX')[idx_name]
+            idx_type = ','.join(it for it in ElasticSettings.attrs().get('SEARCH').get('IDX_TYPES')[idx_name])
 
         aggs = Aggs(Agg("categories", "terms", {"field": "_type", "size": 0}))
-        highlight = Highlight(fields)
-        search_query = ElasticQuery.query_string(query, sources=sources, highlight=highlight, fields=fields)
-        elastic = Search(search_query=search_query, aggs=aggs, search_from=0, size=20, idx=idx)
+        highlight = Highlight(highlight_fields, pre_tags="<strong>", post_tags="</strong>")
+        search_query = ElasticQuery.query_string(query, fields=fields, sources=source_filter, highlight=highlight)
+        elastic = Search(search_query=search_query, aggs=aggs, search_from=0, size=50,
+                         idx=idx, idx_type=idx_type)
         result = elastic.search()
-        context = {'data': result.docs, 'query': query}
+        mappings = elastic.get_mapping()
+        context = {'data': result.docs,
+                   'query': query,
+                   'fields': fields,
+                   'mappings': mappings[idx],
+                   'hits_total': result.hits_total}
 
         return render(request, 'search_engine/result.html', context,
                       content_type='text/html')
     else:
-        search = Search(idx=idx)
-        mapping = search.get_mapping()
-        context = {'data': mapping[idx]}
+        context = {'index': ElasticSettings.attrs().get('SEARCH').get('IDX_TYPES').keys()}
         return render(request, 'search_engine/search.html', context,
                       content_type='text/html')
