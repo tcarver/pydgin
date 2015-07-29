29110dc0e803bbeb92929fbd426ca23d6e206c71
tcarver <tjc29@duffield>
2015-07-29 15:40:20 +0100

    add search engine js for autocompletion

---
+++ pydgin/local_apps/search_engine/static/search_engine/search_engine.js
@@ -0,0 +1,28 @@
+(function( search_engine, $, undefined ) { 
+
+	search_engine.autocomplete = function (searchId) {
+		$( "#" + searchId ).autocomplete({
+	        source: function( request, response ) {
+	          $.ajax({
+	            url: "/search/suggest",
+	            contentType: 'application/json',
+	            data: {
+	              term: request.term,
+	              idx: $( "#idx" ).val(),
+	            },
+	            success: function( data ) {
+	              response( data.data );
+	            }
+	          });
+	        },
+	        minLength: 2,
+	        open: function() {
+	          $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+	        },
+	        close: function() {
+	          $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+	        }
+	      });
+	}
+
+}( window.search_engine = window.search_engine || {}, jQuery ));
