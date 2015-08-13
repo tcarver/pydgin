f80c4ddc2c7cce9893da25d90931af640db0cc73
tcarver <tjc29@duffield>
2015-08-13 10:16:24 +0100

    test search examples return results

--- pydgin/local_apps/search_engine/tests/tests_search_engine.py
+++ pydgin/local_apps/search_engine/tests/tests_search_engine.py
@@ -33,6 +33,7 @@ class SearchEngineTest(TestCase):
                     link_url = url + item[:end]
                     resp = self.client.get(link_url)
                     self.assertEqual(resp.status_code, 200, msg=link_url)
+                    self.assertGreater(resp.context['hits_total'], 0)
 
     def test_search(self):
         ''' Test the search. '''
@@ -40,7 +41,7 @@ class SearchEngineTest(TestCase):
         resp = self.client.get(url, {"idx": "ALL", "query": "+PTPN22 +todd"})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context['query'], "+PTPN22 +todd")
-        self.assertGreaterEqual(resp.context['hits_total'], 0)
+        self.assertGreater(resp.context['hits_total'], 0)
         self.assertTemplateUsed(resp, 'search_engine/result.html')
 
     def test_search_filters(self):
