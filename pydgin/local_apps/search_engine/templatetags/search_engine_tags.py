9d536b019d6b7f8930345c0ff018129fc037f80a
tcarver <tjc29@duffield>
2015-08-24 16:59:06 +0100

    move listsort to elastic filter_tags

--- pydgin/local_apps/search_engine/templatetags/search_engine_tags.py
+++ pydgin/local_apps/search_engine/templatetags/search_engine_tags.py
@@ -1,7 +1,6 @@
 ''' Template tags for the search engine app. '''
 from django import template
 from elastic.elastic_settings import ElasticSettings
-import collections
 
 register = template.Library()
 
@@ -10,22 +9,3 @@ register = template.Library()
 def show_search_engine():
     ''' Template inclusion tag to render search engine form. '''
     return {'index': ElasticSettings.attrs().get('SEARCH').get('IDX_TYPES').keys()}
-
-
-@register.filter(name='sort')
-def listsort(value):
-    ''' Sort list and iterable arguments. '''
-    if isinstance(value, list):
-        try:
-            new_list = list(value)
-            new_list.sort()
-            return new_list
-        except TypeError:
-            # sorting a list of dictionaries?
-            if 'key' in value[0]:
-                return sorted(value, key=lambda k: k['key'])
-    elif isinstance(value, collections.Iterable):
-        return sorted(value)
-    return value
-
-listsort.is_safe = True
