e7ac1668107329ce40ae58c7b51d8a6ba497cdb4
tcarver <tjc29@duffield>
2015-08-26 12:56:33 +0100

    add db_link() and is_list()

--- pydgin/local_apps/gene/templatetags/gene_tags.py
+++ pydgin/local_apps/gene/templatetags/gene_tags.py
@@ -1,6 +1,23 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
+
+
+@register.filter
+def db_link(db):
+    ''' Look up a URL for a given database. '''
+    settings
+    db_names = settings.URL_LINKS.keys()
+    if db in db_names:
+        return settings.URL_LINKS[db]
+    return ""
+
+
+@register.filter
+def is_list(val):
+    ''' Is the value an instance of a list. '''
+    return isinstance(val, list)
 
 
 @register.inclusion_tag('gene/pub_section.html')
