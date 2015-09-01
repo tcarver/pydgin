92fb43a471ef4ca15a2b3504e6f6527c70e69904
prem <premanand.achuthan@cimr.cam.ac.uk>
2015-09-01 12:33:31 +0100

    updates for search permissions

--- pydgin/local_apps/search_engine/urls.py
+++ pydgin/local_apps/search_engine/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from search_engine import views
 
-urlpatterns = [
-               url(r'^', views.search_page, name='search_page'),
+urlpatterns = [url(r'^', views.search_page, name='search_page'),
                ]
