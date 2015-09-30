3669fed993f3442cdab48fa973760cdcf2300d8c
tcarver <tjc29@duffield>
2015-09-30 16:55:11 +0100

    add show_studies_section()

--- pydgin/local_apps/gene/templatetags/gene_tags.py
+++ pydgin/local_apps/gene/templatetags/gene_tags.py
@@ -27,6 +27,13 @@ def show_pub_section(gene):
     return {'gene': gene}
 
 
+@register.inclusion_tag('gene/studies_section.html')
+def show_studies_section(ens_id):
+    ''' Template inclusion tag to render an studies section given a
+    ensembl ID. '''
+    return {'ens_id': ens_id}
+
+
 @register.inclusion_tag('gene/interactions_section.html')
 def show_interactions_section(ens_id):
     ''' Template inclusion tag to render an interaction section given a
