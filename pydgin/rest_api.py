f941f01beda275899290a0b623cd5e79958036be
tcarver <tim.carver@cimr.cam.ac.uk>
2016-01-13 16:06:56 +0000

    add error field

--- pydgin/rest_api.py
+++ pydgin/rest_api.py
@@ -39,9 +39,11 @@ class LDSerializer(serializers.Serializer):
         marker2 = serializers.CharField(help_text='marker 2')
         dprime = serializers.FloatField(help_text='D prime')
         rsquared = serializers.FloatField(help_text='R squared')
-        MAF = serializers.FloatField(required=False)
-        position = serializers.IntegerField(required=False)
+        MAF = serializers.FloatField(required=False, help_text='Minor allele frequency')
+        position = serializers.IntegerField(required=False, help_text='Position of variant 2')
+
     ld = serializers.ListField(child=LD())
+    error = serializers.CharField(required=False)
 
 
 class LDViewSet(RetrieveLDMixin, ListLDMixin, viewsets.ReadOnlyModelViewSet):
