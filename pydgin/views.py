f7fdc9a03798091853a64dbac97e785d6b7736c2
Ellen Schofield <ellen.schofield@cimr.cam.ac.uk>
2016-01-18 09:29:43 +0000

    re-named html page

--- pydgin/views.py
+++ pydgin/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     ''' Renders a front page. '''
-    return render(request, 'www-fp.html', {}, content_type='text/html')
+    return render(request, 'front_page.html', {}, content_type='text/html')
