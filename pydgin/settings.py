fa1f981dbff54eb508242283da69fdfb238cc53c
Premanand Achuthan <prem.apa@gmail.com>
2016-01-18 14:53:04 +0000

    added criteria to pydgin settings

--- pydgin/settings.py
+++ pydgin/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'criteria',
     'data_pipeline',
     'elastic',
     'search_engine',
