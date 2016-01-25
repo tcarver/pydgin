0439c3ff0d826322b8e601a2d7ca5d58efca8e98
Ellen Schofield <ellen.schofield@cimr.cam.ac.uk>
2016-01-25 15:01:45 +0000

    setup to use test data and override elastic settings

--- pydgin/local_apps/disease/tests/test_disease.py
+++ pydgin/local_apps/disease/tests/test_disease.py
@@ -3,26 +3,30 @@ from django.test import TestCase
 from elastic.elastic_settings import ElasticSettings
 import logging
 from disease import utils
+from django.test.utils import override_settings
+from pydgin.tests.data.settings_idx import PydginTestSettings
 
 logger = logging.getLogger(__name__)
 
 
+@override_settings(ELASTIC=PydginTestSettings.OVERRIDE_SETTINGS)
 class DiseaseTest(TestCase):
     idx = ''
 
     @classmethod
-    def setUpClass(cls):
+    def setUp(cls):
         DiseaseTest.idx = ElasticSettings.idx('DISEASE')
+        PydginTestSettings.setupIdx(['DISEASE'])
 
     @classmethod
-    def tearDownClass(cls):
-        pass
+    def tearDown(cls):
+        PydginTestSettings.tearDownIdx(['DISEASE'])
 
     def test_all_diseases(self):
         ''' Test gteting all diseases on the site '''
         (main, other) = utils.Disease.get_site_diseases()
         self.assertEqual(12, len(main), "12 main diseases found when searching for all diseases")
-        self.assertEqual(7, len(other), "7 other diseases found when searching for all diseases")
+        self.assertEqual(1, len(other), "7 other diseases found when searching for all diseases")
 
     def test_main_diseases(self):
         ''' Test gteting all diseases on the site '''
@@ -34,4 +38,4 @@ class DiseaseTest(TestCase):
         ''' Test gteting all diseases on the site '''
         (main, other) = utils.Disease.get_site_diseases(tier=1)
         self.assertEqual(0, len(main), "12 main diseases found when searching for other diseases")
-        self.assertEqual(7, len(other), "0 other diseases found when searching for other diseases")
+        self.assertEqual(1, len(other), "0 other diseases found when searching for other diseases")
