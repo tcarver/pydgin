9e9ca4f9eb7c26af9660a4f6a72d546a82c826e1
Ellen Schofield <ellen.schofield@cimr.cam.ac.uk>
2016-01-27 12:38:12 +0000

    added support for neater urls as well as GET parameter-based urls

--- pydgin/local_apps/region/urls.py
+++ pydgin/local_apps/region/urls.py
@@ -1,8 +1,8 @@
 ''' Define region urls. '''
 from django.conf.urls import url
 from region import views
-from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.region_page, name='region_page'),
+    url(r'^$', views.region_page_params, name='region_page_params'),
+    url(r'^(?P<region>.*)/$', views.region_page, name='region_page')
 ]
