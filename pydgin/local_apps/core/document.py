f657d1360a9b6b5c362cd8b9fe0af66198fd1be2
Ellen Schofield <ellen.schofield@cimr.cam.ac.uk>
2016-01-27 12:13:20 +0000

    added get_position def for documents to be inherited

--- pydgin/local_apps/core/document.py
+++ pydgin/local_apps/core/document.py
@@ -14,3 +14,11 @@ class FeatureDocument(Document):
     def get_name(self):
         ''' Overridden get feature name. '''
         raise NotImplementedError("Inheriting class should implement this method")
+
+    def get_position(self, build=38):
+        '''
+        Overridden get feature position by build.
+        @type  build: integer
+        @keyword build: NCBI build to return (default: 38).
+         '''
+        raise NotImplementedError("Inheriting class should implement this method")
