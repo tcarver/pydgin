c6b5e1a280f9421dc7ef70842ae35a49e85d9ddc
tcarver <tim.carver@cimr.cam.ac.uk>
2016-01-27 15:44:04 +0000

    more tests

--- pydgin/local_apps/core/tests/test_feature_doc.py
+++ pydgin/local_apps/core/tests/test_feature_doc.py
@@ -6,22 +6,23 @@ from elastic.search import ElasticQuery, Search
 from elastic.query import Query
 from gene.document import GeneDocument
 from disease.document import DiseaseDocument
+from core.document import PydginDocument
 
 
 @override_settings(ELASTIC=PydginTestSettings.OVERRIDE_SETTINGS)
 def setUpModule():
     ''' create elastic indices for querying '''
-    PydginTestSettings.setupIdx(['GENE', 'DISEASE'])
+    PydginTestSettings.setupIdx(['GENE', 'DISEASE', 'PUBLICATION'])
 
 
 @override_settings(ELASTIC=PydginTestSettings.OVERRIDE_SETTINGS)
 def tearDownModule():
     ''' Remove test indices '''
-    PydginTestSettings.tearDownIdx(['GENE', 'DISEASE'])
+    PydginTestSettings.tearDownIdx(['GENE', 'DISEASE', 'PUBLICATION'])
 
 
 @override_settings(ELASTIC=PydginTestSettings.OVERRIDE_SETTINGS)
-class FeatureDocumentTest(TestCase):
+class PydginDocumentTest(TestCase):
 
     def test_doc(self):
         ''' Test return correct type of FeatureDocument. '''
@@ -29,8 +30,7 @@ class FeatureDocumentTest(TestCase):
         idx_type = PydginTestSettings.IDX['GENE']['indexType']
         res = Search(search_query=ElasticQuery(Query.match_all(), sources=['symbol']),
                      idx=idx, idx_type=idx_type, size=2).search()
-        docs = res.docs
-        for doc in docs:
+        for doc in res.docs:
             self.assertTrue(isinstance(doc, GeneDocument))
 
     def test_doc2(self):
@@ -38,8 +38,20 @@ class FeatureDocumentTest(TestCase):
         idx = PydginTestSettings.IDX['GENE']['indexName'] + ',' + PydginTestSettings.IDX['DISEASE']['indexName']
         res = Search(search_query=ElasticQuery(Query.match_all(), sources=['symbol', 'code']),
                      idx=idx, size=40).search()
-        docs = res.docs
-        for doc in docs:
+        for doc in res.docs:
             self.assertTrue(isinstance(doc, GeneDocument) or isinstance(doc, DiseaseDocument))
             if isinstance(doc, DiseaseDocument):
+                self.assertTrue(hasattr(doc, 'code'))
+
+    def test_doc3(self):
+        ''' Test return correct type of PydginDocument/FeatureDocument using multiple index search. '''
+        idx = PydginTestSettings.IDX['DISEASE']['indexName'] + ',' + PydginTestSettings.IDX['PUBLICATION']['indexName']
+        res = Search(search_query=ElasticQuery(Query.match_all(), sources=['code', 'pmid']),
+                     idx=idx, size=40).search()
+
+        for doc in res.docs:
+            self.assertTrue(isinstance(doc, PydginDocument) or isinstance(doc, DiseaseDocument))
+            if isinstance(doc, PydginDocument):
+                self.assertTrue(hasattr(doc, 'pmid'))
+            else:
                 self.assertTrue(hasattr(doc, 'code'))
