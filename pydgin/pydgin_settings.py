1589da88881548edfe5a9952900772f0830649a2
tcarver <tim.carver@cimr.cam.ac.uk>
2016-02-11 09:54:09 +0000

    add pydgin settings

---
+++ pydgin/pydgin_settings.py
@@ -0,0 +1,15 @@
+''' Pydgin project settings. '''
+from collections import OrderedDict
+DEFAULT_BUILD = 38
+
+PAGE_SECTIONS = {
+    'GeneView': OrderedDict([
+        ('overview', True),
+        ('study', True),
+        ('publication', True),
+        ('interactions', True),
+        ('genesets', True),
+        ('phenotype links', True)]),
+    'Marker': OrderedDict([
+        ('overview', True)])
+}
