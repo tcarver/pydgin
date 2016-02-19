5f62a6a602f4a8d811fa2694b659f86efa6fbba2
tcarver <tim.carver@cimr.cam.ac.uk>
2016-02-19 11:28:15 +0000

    js test

--- pydgin/local_apps/marker/urls.py
+++ pydgin/local_apps/marker/urls.py
@@ -1,9 +1,14 @@
 ''' Define search engine urls. '''
+from django.conf import settings
 from django.conf.urls import url
 
+from marker import views
 from marker.views import MarkerView
 
 
 urlpatterns = [
         url(r'^$', MarkerView.as_view(), name='marker_page')
     ]
+
+if settings.DEBUG or settings.TESTMODE:
+    urlpatterns.append(url(r'^js_test/$', views.js_test, name='js_test'))
--- pydgin/local_apps/marker/views.py
+++ pydgin/local_apps/marker/views.py
@@ -1,8 +1,11 @@
 ''' Marker page view. '''
 import logging
 
+from django.conf import settings
 from django.contrib import messages
 from django.http import Http404
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import TemplateView
 
 from core.document import PydginDocument
@@ -95,3 +98,11 @@ def _get_marker_build(idx_name):
     except (KeyError, SettingsError, TypeError):
         logger.error('Marker build not identified from ELASTIC settings.')
         return ''
+
+
+@ensure_csrf_cookie
+def js_test(request):
+    ''' Renders a gene page. '''
+    if not (settings.DEBUG or settings.TESTMODE):
+        raise Http404()
+    return render(request, 'js_test/ld.html', {}, content_type='text/html')
