eaf5fdf0aae6ebf040e02ec26d8fd560dc6a5814
tcarver <tim.carver@cimr.cam.ac.uk>
2016-02-22 15:46:24 +0000

    define section_name

--- pydgin/local_apps/core/views.py
+++ pydgin/local_apps/core/views.py
@@ -8,8 +8,9 @@ class SectionMixin(object):
 
     def get_context_data(self, **kwargs):
         context = super(SectionMixin, self).get_context_data(**kwargs)
+        sections_name = self.sections_name if hasattr(self, 'sections_name') else self.__class__.__name__
         context['sections'] = OrderedDict([(k, v) for k, v in
-                                           settings.PAGE_SECTIONS[self.__class__.__name__].items()
+                                           settings.PAGE_SECTIONS[sections_name].items()
                                            if (isinstance(v, dict) and v['show']) or v])
         return context
 
