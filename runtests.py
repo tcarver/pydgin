9308e9b6ddbe9cc760f705d45cc1c1331c233fca
tcarver <tim.carver@cimr.cam.ac.uk>
2016-02-24 10:44:08 +0000

    tidy app tests

--- runtests.py
+++ runtests.py
@@ -12,15 +12,16 @@ if __name__ == "__main__":
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     settings.TESTMODE = True
-    failures = test_runner.run_tests(['auth_test.tests',
-                                      'core.tests',
-                                      'disease.tests',
-                                      'elastic.tests',
-                                      'gene.tests',
-                                      'marker.tests',
-                                      'pydgin_auth.tests',
-                                      'pydgin.tests',
-                                      'region.tests',
-                                      'search_engine.tests',
-                                      'study.tests'])
+    failures = test_runner.run_tests([
+        'auth_test.tests',
+        'core.tests',
+        'disease.tests',
+        'elastic.tests',
+        'gene.tests',
+        'marker.tests',
+        'pydgin_auth.tests',
+        'pydgin.tests',
+        'region.tests',
+        'search_engine.tests',
+        'study.tests'])
     sys.exit(bool(failures))
