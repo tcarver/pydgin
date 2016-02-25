f329bedb41db7bf1e6b261b8623298af79a0b6cc
tcarver <tim.carver@cimr.cam.ac.uk>
2016-02-25 10:13:45 +0000

    remove MarkerViewParams

--- pydgin/local_apps/marker/urls.py
+++ pydgin/local_apps/marker/urls.py
@@ -2,13 +2,13 @@
 from django.conf import settings
 from django.conf.urls import url
 
-from marker.views import MarkerView, MarkerViewParams, JSTestView
+from marker.views import MarkerView, JSTestView
 
 
 urlpatterns = [
-    url(r'^$', MarkerViewParams.as_view(), name='marker_page_params'),
-   url(r'^(?P<marker>.*)/$', MarkerView.as_view(), name='marker_page')
+    url(r'^$', MarkerView.as_view(), name='marker_page_params'),
+    url(r'^(?P<marker>.*)/$', MarkerView.as_view(), name='marker_page')
 ]
 
 if settings.DEBUG or settings.TESTMODE:
-    urlpatterns.append(url(r'^js_test/$', JSTestView.as_view(), name='js_test'))
+    urlpatterns.append(url(r'^js_test/$', JSTestView.as_view(), name='marker_js_test'))
