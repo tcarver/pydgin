7e89c54be5d82c6bfd4be97207ebb7d454588676
tcarver <tim.carver@cimr.cam.ac.uk>
2016-03-14 09:50:52 +0000

    update functional info

--- pydgin/local_apps/marker/templatetags/marker_tags.py
+++ pydgin/local_apps/marker/templatetags/marker_tags.py
@@ -6,6 +6,7 @@ from django import template
 from elastic.elastic_settings import ElasticSettings
 from elastic.query import BoolQuery, Filter, Query
 from elastic.search import Search, ElasticQuery
+from collections import OrderedDict
 
 
 register = template.Library()
@@ -43,14 +44,20 @@ def marker_functional_info(marker_doc):
         if 'VP' not in info_parts:
             return None
         vp = info_parts['VP']
+
         if vp.startswith('0x'):
             vp = vp[2:]
+
+        h_size = len(vp) * 4
+        info_bits = (bin(int(vp, 16))[2:]).zfill(h_size)
+        F2_1 = info_bits[24:32]     # gene function properties
+        F2_2 = info_bits[32:40]
 
         # F0 = bin(int(vp[0:2], 16))[2:].rjust(8, '0')     # version encoding
         # F1_1 = bin(int(vp[2:4], 16))[2:].rjust(8, '0')   # resource link properties
         # F1_2 = bin(int(vp[4:6], 16))[2:].rjust(8, '0')
-        F2_1 = bin(int(vp[6:8], 16))[2:].rjust(8, '0')     # gene function properties
-        F2_2 = bin(int(vp[8:10], 16))[2:].rjust(8, '0')
+        # F2_1 = bin(int(vp[6:8], 16))[2:].rjust(8, '0')     # gene function properties
+        # F2_2 = bin(int(vp[8:10], 16))[2:].rjust(8, '0')
         # F3 = bin(int(vp[10:12], 16))[2:].rjust(8, '0')   # mapping
         # F4 = bin(int(vp[12:14], 16))[2:].rjust(8, '0')   # allele frequency
         # F5 = bin(int(vp[14:16], 16))[2:].rjust(8, '0')   # genotype
@@ -59,23 +66,23 @@ def marker_functional_info(marker_doc):
         # F8 = bin(int(vp[20:22], 16))[2:].rjust(8, '0')   # variation class
         # F9 = bin(int(vp[22:24], 16))[2:].rjust(8, '0')   # quality check
 
-        functional_f2 = {
-            'in_gene_seg': F2_1[7] == '1',
-            'in_5_prime_gene_region': F2_1[6] == '1',
-            'in_3_prime_gene_region': F2_1[5] == '1',
-            'in_intron': F2_1[4] == '1',
-            'in_donor_splice_site': F2_1[3] == '1',
-            'in_acceptor_splice_site': F2_1[2] == '1',
-            'in_5prime_utr': F2_1[1] == '1',
-            'in_3prime_utr': F2_1[0] == '1',
+        functional_f2 = OrderedDict([
+            ('in gene seg', F2_1[7] == '1'),
+            ('in 5prime gene region', F2_1[6] == '1'),
+            ('in 3prime gene region', F2_1[5] == '1'),
+            ('in intron', F2_1[4] == '1'),
+            ('in donor splice site', F2_1[3] == '1'),
+            ('in acceptor_splice_site', F2_1[2] == '1'),
+            ('in 5prime utr', F2_1[1] == '1'),
+            ('in 3prime utr', F2_1[0] == '1'),
 
-            'has_synonymous': F2_2[7] == '1',
-            'has_reference': F2_2[6] == '1',
-            'has_stop_gain': F2_2[5] == '1',
-            'has_non_synonymous_missense': F2_2[4] == '1',
-            'has_non_synonymous_frameshift': F2_2[3] == '1',
-            'has_stop_loss': F2_2[2] == '1'
-        }
+            ('has synonymous', F2_2[7] == '1'),
+            ('has reference', F2_2[6] == '1'),
+            ('has stop gain', F2_2[5] == '1'),
+            ('has non-synonymous missense', F2_2[4] == '1'),
+            ('has non-synonymous frameshift', F2_2[3] == '1'),
+            ('has stop loss', F2_2[2] == '1')
+        ])
 
         return functional_f2
     return None
