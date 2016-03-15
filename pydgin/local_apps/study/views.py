b3677b1bc2cda8b8dc7a25048eb4ee0b41955900
tcarver <tim.carver@cimr.cam.ac.uk>
2016-03-15 14:05:53 +0000

    add study section view

--- pydgin/local_apps/study/views.py
+++ pydgin/local_apps/study/views.py
@@ -1,12 +1,14 @@
 ''' Study views. '''
 from django.contrib import messages
 from django.http import Http404
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 
 from elastic.elastic_settings import ElasticSettings
-from elastic.query import Query
+from elastic.query import Query, Filter
 from elastic.search import ElasticQuery, Search
 from study.document import StudyDocument
+from django.http.response import JsonResponse
+from gene import utils
 
 
 class StudyView(TemplateView):
@@ -34,3 +36,50 @@ class StudyView(TemplateView):
             context['title'] = names
             return context
         raise Http404()
+
+
+class StudySectionView(View):
+    ''' Study section for gene/marker/region. '''
+
+    def post(self, request, *args, **kwargs):
+        ens_id = self.request.POST.get('ens_id')
+        marker = self.request.POST.get('marker')
+        if ens_id:
+            sfilter = Filter(Query.query_string(ens_id, fields=["genes"]).query_wrap())
+        elif marker:
+            sfilter = Filter(Query.query_string(marker, fields=["marker"]).query_wrap())
+
+        query = ElasticQuery.filtered(Query.match_all(), sfilter)
+        elastic = Search(query, idx=ElasticSettings.idx('REGION', 'STUDY_HITS'), size=500)
+        study_hits = elastic.get_json_response()['hits']
+
+        ens_ids = []
+        pmids = []
+        for hit in study_hits['hits']:
+            if 'pmid' in hit['_source']:
+                pmids.append(hit['_source']['pmid'])
+            for ens_id in hit['_source']['genes']:
+                ens_ids.append(ens_id)
+        docs = utils.get_gene_docs_by_ensembl_id(ens_ids, ['symbol'])
+        pub_docs = utils.get_pub_docs_by_pmid(pmids, sources=['authors.name', 'journal'])
+
+        for hit in study_hits['hits']:
+            genes = {}
+            for ens_id in hit['_source']['genes']:
+                try:
+                    genes[ens_id] = getattr(docs[ens_id], 'symbol')
+                except KeyError:
+                    genes = {ens_id: ens_id}
+            hit['_source']['genes'] = genes
+            if 'pmid' in hit['_source']:
+                pmid = hit['_source']['pmid']
+                try:
+                    authors = getattr(pub_docs[pmid], 'authors')
+                    journal = getattr(pub_docs[pmid], 'journal')
+                    hit['_source']['pmid'] = \
+                        {'pmid': pmid,
+                         'author': authors[0]['name'].rsplit(None, 1)[-1],
+                         'journal': journal}
+                except KeyError:
+                    hit['_source']['pmid'] = {'pmid': pmid}
+        return JsonResponse(study_hits)
