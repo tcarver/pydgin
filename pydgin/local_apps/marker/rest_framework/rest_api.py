0eba5fbf5774865154603125b74b4670a3ead889
tcarver <tim.carver@cimr.cam.ac.uk>
2016-03-29 13:34:44 +0100

    add populations docs

--- pydgin/local_apps/marker/rest_framework/rest_api.py
+++ pydgin/local_apps/marker/rest_framework/rest_api.py
@@ -29,34 +29,42 @@ class LDViewSet(ListLDMixin, viewsets.ReadOnlyModelViewSet):
               description: marker (e.g. rs2476601) to identify markers in LD with.
               required: true
               type: string
+              paramType: query
             - name: m2
               description: optional marker to calculate LD for marker1, Defaults to NULL.
               required: false
               type: string
+              paramType: query
             - name: window_size
               description: window size to look for markers in LD, Defaults to 1000000.
               required: false
               type: integer
+              paramType: query
             - name: dprime
               description: dprime to use, Defaults to 0.
               required: false
               type: float
+              paramType: query
             - name: rsq
               description: R square threshold, Defaults to 0.8.
               required: false
               type: float
+              paramType: query
             - name: build
               description: Genome build, e.g grch38.
               required: false
               type: string
+              paramType: query
             - name: maf
               description: if TRUE report the MAF in the result.
               required: false
               type: boolean
+              paramType: query
             - name: pos
               description: if TRUE report the position in the result.
               required: false
               type: boolean
+              paramType: query
     """
     serializer_class = LDSerializer
     lookup_url_kwarg = "m1"
@@ -79,6 +87,16 @@ class PopulationsSerializer(serializers.Serializer):
 
 
 class PopulationsViewSet(ListPopulationMixin, viewsets.ReadOnlyModelViewSet):
-    """ Returns population stats for a given variant. """
+    """ Returns population stats for a given variant.
+    ---
+    list:
+        response_serializer: PopulationsSerializer
+        parameters:
+            - name: marker
+              description: marker (e.g. rs2476601) to identify markers in LD with.
+              required: false
+              type: string
+              paramType: query
+    """
     serializer_class = PopulationsSerializer
     filter_fields = ('marker')
