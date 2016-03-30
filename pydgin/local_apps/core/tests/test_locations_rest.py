89e188dbb93bd6a5da7e44434a79d77acdf4cc42
tcarver <tim.carver@cimr.cam.ac.uk>
2016-03-30 11:22:02 +0100

    add region to test

--- pydgin/local_apps/core/tests/test_locations_rest.py
+++ pydgin/local_apps/core/tests/test_locations_rest.py
@@ -10,12 +10,16 @@ class LocationsRestTest(TestCase):
     def test_ld_list(self):
         ''' Test retrieving markers in LD of given marker. '''
         try:
-            url = reverse('rest:locations-list') + "?format=json&m1=rs10774624"
+            url = reverse('rest:locations-list')
             response = self.client.get(url, data={'feature': 'rs2476601', 'build': 'hg38'})
             loc = json.loads(response.content.decode("utf-8"))[0]
             self.assertGreater(len(loc), 0, 'results found')
 
             response = self.client.get(url, data={'feature': 'PTPN22', 'build': 'hg38'})
+            loc = json.loads(response.content.decode("utf-8"))[0]
+            self.assertGreater(len(loc), 0, 'results found')
+
+            response = self.client.get(url, data={'feature': '1p13.2', 'build': 'hg38'})
             loc = json.loads(response.content.decode("utf-8"))[0]
             self.assertGreater(len(loc), 0, 'results found')
         except (KeyError, NoReverseMatch):
