8a541fc6ff92e50cee130e7fd7ce72f8c68711ce
tcarver <tim.carver@cimr.cam.ac.uk>
2016-04-01 10:23:52 +0100

    add recaptcha()

--- pydgin/local_apps/core/context_processors.py
+++ pydgin/local_apps/core/context_processors.py
@@ -14,3 +14,11 @@ def appname(request):
         return {'appname': request.path.split('/')[1]}
     except Exception as e:
         print(e.message)
+
+
+def recaptcha(request):
+    ''' Recaptcha for contact form. '''
+    try:
+        return {'RECAPTCHA_KEY': settings.RECAPTCHA_KEY}
+    except Exception as e:
+        print(e.message)
