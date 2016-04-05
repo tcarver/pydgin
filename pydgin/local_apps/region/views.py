ac8c0739900db4ccfed6b79bceb1b815e61580f1
tcarver <tim.carver@cimr.cam.ac.uk>
2016-04-05 10:56:44 +0100

    optimisations

--- pydgin/local_apps/region/views.py
+++ pydgin/local_apps/region/views.py
@@ -75,7 +75,7 @@ class RegionTableView(TemplateView):
 
     @classmethod
     def get_regions(cls, request, dis, context):
-        is_authenticated = False
+        # is_authenticated = False
         build = pydgin_settings.DEFAULT_BUILD
         elastic_url = ElasticSettings.url()
 
@@ -111,43 +111,25 @@ class RegionTableView(TemplateView):
                 'seqid': 'chr'+getattr(r, "seqid")
             }
             hits = getattr(r, "hits")
-            # hits_query = ElasticQuery(Query.ids(hits))
-            hits_query = ElasticQuery.filtered(Query.ids(hits),
-                                               Filter(BoolQuery(should_arr=[Query.missing_terms("field", "group_name")]
-                                                                )))
+            hits_query = ElasticQuery.filtered(
+                            Query.ids(hits),
+                            Filter(BoolQuery(should_arr=[Query.missing_terms("field", "group_name")])))
             hits_res = Search(hits_query, idx=ElasticSettings.idx('REGION', 'STUDY_HITS'),
                               aggs=Aggs(build_info_agg), size=len(hits)).search()
             if hits_res.hits_total > 0:
                 diseases = [dis]
                 build_info = getattr(hits_res.aggs['build_info'], 'filtered_result')
-                region['start'] = str(locale.format("%d",  int(build_info['region_start']['value']), grouping=True))
-                region['end'] = str(locale.format("%d",  int(build_info['region_end']['value']), grouping=True))
-                for h in hits_res.docs:
-                    setattr(h, "p_value", None)
-                    if getattr(h, "p_values")['combined'] is not None:
-                        setattr(h, "p_value", float(getattr(h, "p_values")['combined']))
-                        setattr(h, "p_val_src", "C")
-                    elif getattr(h, "p_values")['discovery'] is not None:
-                        setattr(h, "p_value", float(getattr(h, "p_values")['discovery']))
-                        setattr(h, "p_val_src", "D")
+                regions_start = int(build_info['region_start']['value'])
+                regions_stop = int(build_info['region_end']['value'])
 
-                    setattr(h, "odds_ratio", None)
-                    setattr(h, "or_bounds", None)
-                    if getattr(h, "odds_ratios")['combined']['or'] != None:
-                        setattr(h, "odds_ratio", getattr(h, "odds_ratios")['combined']['or'])
-                        setattr(h, "or_src", "C")
-                        if getattr(h, "odds_ratios")['combined']['upper'] != None:
-                            setattr(h, "or_bounds", "("+getattr(h, "odds_ratios")['combined']['lower']+"-"+getattr(h, "odds_ratios")['combined']['upper']+")")
-                    if getattr(h, "odds_ratios")['discovery']['or'] != None:
-                        setattr(h, "odds_ratio", getattr(h, "odds_ratios")['discovery']['or'])
-                        setattr(h, "or_src", "D")
-                        if getattr(h, "odds_ratios")['discovery']['upper'] != None:
-                            setattr(h, "or_bounds", "("+getattr(h, "odds_ratios")['discovery']['lower']+"-"+getattr(h, "odds_ratios")['discovery']['upper']+")")
+                region['start'] = str(locale.format("%d",  regions_start, grouping=True))
+                region['end'] = str(locale.format("%d",  regions_stop, grouping=True))
 
-                region['hits'] = hits_res.docs
-                region['markers'] = list(set([h.marker for h in hits_res.docs]))
+                r_docs = hits_res.docs
+                region['hits'] = _process_hits(r_docs, diseases)
+                region['markers'] = list(set([h.marker for h in r_docs]))
                 cand_genes = {}
-                for h in hits_res.docs:
+                for h in r_docs:
                     if h.genes is not None:
                         cand_genes.update(gene.utils.get_gene_docs_by_ensembl_id(h.genes))
                 region['cand_genes'] = cand_genes
@@ -160,7 +142,8 @@ class RegionTableView(TemplateView):
                 for doc in stats_result.docs:
                     idx = doc.index()
                     idx_type = doc.type()
-                    meta_response = Search.elastic_request(elastic_url, idx + '/' + idx_type + '/_mapping', is_post=False)
+                    meta_response = Search.elastic_request(elastic_url, idx + '/' + idx_type + '/_mapping',
+                                                           is_post=False)
                     elastic_meta = json.loads(meta_response.content.decode("utf-8"))
                     meta_info = elastic_meta[idx]['mappings'][idx_type]['_meta']
                     setattr(doc, "disease", meta_info['disease'])
@@ -173,60 +156,20 @@ class RegionTableView(TemplateView):
                 '''@TODO add authentication here.'''
                 region['marker_stats'] = stats_result.docs
 
-                other_hits_query = ElasticQuery(BoolQuery(must_arr=[RangeQuery("tier", lte=2), Query.terms("marker", region['markers'])],
-                                                          must_not_arr=[Query.terms("dil_study_id", study_ids), Query.term("disease", dis.lower())]))
-                other_hits_res = Search(other_hits_query, idx=ElasticSettings.idx('REGION', 'STUDY_HITS'), size=100).search()
-                for h in other_hits_res.docs:
-                    if h.disease is not None:
-                        diseases.append(h.disease)
+                other_hits_query = ElasticQuery(
+                        BoolQuery(must_arr=[RangeQuery("tier", lte=2), Query.terms("marker", region['markers'])],
+                                  must_not_arr=[Query.terms("dil_study_id", study_ids),
+                                                Query.term("disease", dis.lower())]))
+                other_hits = Search(other_hits_query, idx=ElasticSettings.idx('REGION', 'STUDY_HITS'),
+                                    size=100).search()
 
-                    setattr(h, "p_value", None)
-                    if getattr(h, "p_values")['combined'] is not None:
-                        setattr(h, "p_value", float(getattr(h, "p_values")['combined']))
-                        setattr(h, "p_val_src", "C")
-                    elif getattr(h, "p_values")['discovery'] is not None:
-                        setattr(h, "p_value", float(getattr(h, "p_values")['discovery']))
-                        setattr(h, "p_val_src", "D")
+                region['extra_markers'] = _process_hits(other_hits.docs, diseases)
 
-                    setattr(h, "odds_ratio", None)
-                    setattr(h, "or_bounds", None)
-                    setattr(h, "risk_allele", None)
-                    if getattr(h, "odds_ratios")['combined']['or'] != None:
-                        setattr(h, "odds_ratio", getattr(h, "odds_ratios")['combined']['or'])
-                        setattr(h, "or_src", "C")
-                        if getattr(h, "odds_ratios")['combined']['upper'] != None:
-                            if float(getattr(h, "odds_ratios")['combined']['or']) > 1:
-                                setattr(h, "or_bounds", "("+getattr(h, "odds_ratios")['combined']['lower']+"-"+getattr(h, "odds_ratios")['combined']['upper']+")")
-                            else:
-                                setattr(h, "or_bounds", "("+str(float("{0:.2f}".format(1/float(getattr(h, "odds_ratios")['combined']['upper']))))+"-"+str(float("{0:.2f}".format(1/float(getattr(h, "odds_ratios")['combined']['lower']))))+")")
-
-                    if getattr(h, "odds_ratios")['discovery']['or'] != None:
-                        setattr(h, "odds_ratio", getattr(h, "odds_ratios")['discovery']['or'])
-                        setattr(h, "or_src", "D")
-                        if getattr(h, "odds_ratios")['discovery']['upper'] != None:
-                            if float(getattr(h, "odds_ratios")['discovery']['or']) > 1:
-                                setattr(h, "or_bounds", "("+getattr(h, "odds_ratios")['discovery']['lower']+"-"+getattr(h, "odds_ratios")['discovery']['upper']+")")
-                            else:
-                                setattr(h, "or_bounds", "("+str(float("{0:.2f}".format(1/float(getattr(h, "odds_ratios")['discovery']['upper']))))+"-"+str(float("{0:.2f}".format(1/float(getattr(h, "odds_ratios")['discovery']['lower']))))+")")
-
-                    if getattr(h, "odds_ratio") is not None:
-                        if float(getattr(h, "odds_ratio")) > 1:
-                            setattr(h, "risk_allele", getattr(h, "alleles")['minor'])
-                        else:
-                            setattr(h, "odds_ratio", str(float("{0:.2f}".format(1/float(getattr(h, "odds_ratio"))))))
-                            setattr(h, "risk_allele", getattr(h, "alleles")['major'])
-
-                region['extra_markers'] = other_hits_res.docs
-
-                (region_coding, region_non_coding) = get_genes_for_region(getattr(r, "seqid"),
-                                                                          int(build_info['region_start']['value']),
-                                                                          int(build_info['region_end']['value']))
-                (coding_down, non_coding_down) = get_genes_for_region(getattr(r, "seqid"),
-                                                                      int(build_info['region_start']['value'])-500000,
-                                                                      int(build_info['region_start']['value']))
-                (coding_up, non_coding_up) = get_genes_for_region(getattr(r, "seqid"),
-                                                                  int(build_info['region_end']['value']),
-                                                                  int(build_info['region_end']['value'])+500000)
+                (all_coding, all_non_coding) = get_genes_for_region(getattr(r, "seqid"),
+                                                                    regions_start-500000, regions_stop+500000)
+                (region_coding, coding_up, coding_down) = _region_up_down(all_coding, regions_start, regions_stop)
+                (region_non_coding, non_coding_up, non_coding_down) = \
+                    _region_up_down(all_non_coding, regions_start, regions_stop)
                 genes = {
                     'upstream': {'coding': coding_up, 'non_coding': non_coding_up},
                     'region': {'coding': region_coding, 'non_coding': region_non_coding},
@@ -240,6 +183,73 @@ class RegionTableView(TemplateView):
         context['disease_code'] = [dis]
         context['disease'] = getattr(disease, "name")
         return context
+
+
+def _process_hits(docs, diseases):
+    ''' Process docs to add disease, P-values, odds ratios. '''
+    for h in docs:
+        if h.disease is not None:
+            diseases.append(h.disease)
+
+        setattr(h, "p_value", None)
+        if getattr(h, "p_values")['combined'] is not None:
+            setattr(h, "p_value", float(getattr(h, "p_values")['combined']))
+            setattr(h, "p_val_src", "C")
+        elif getattr(h, "p_values")['discovery'] is not None:
+            setattr(h, "p_value", float(getattr(h, "p_values")['discovery']))
+            setattr(h, "p_val_src", "D")
+
+        setattr(h, "odds_ratio", None)
+        setattr(h, "or_bounds", None)
+        setattr(h, "risk_allele", None)
+        if getattr(h, "odds_ratios")['combined']['or'] != None:
+            setattr(h, "odds_ratio", getattr(h, "odds_ratios")['combined']['or'])
+            setattr(h, "or_src", "C")
+            or_combined = getattr(h, "odds_ratios")['combined']
+            if or_combined['upper'] != None:
+                if float(or_combined['or']) > 1:
+                    setattr(h, "or_bounds", "("+or_combined['lower']+"-"+or_combined['upper']+")")
+                else:
+                    setattr(h, "or_bounds", "("+str(float("{0:.2f}".format(1/float(or_combined['upper'])))) + "-" +
+                            str(float("{0:.2f}".format(1/float(or_combined['lower']))))+")")
+
+        or_discovery = getattr(h, "odds_ratios")['discovery']
+        if or_discovery['or'] != None:
+            setattr(h, "odds_ratio", or_discovery['or'])
+            setattr(h, "or_src", "D")
+            if or_discovery['upper'] != None:
+                if float(or_discovery['or']) > 1:
+                    setattr(h, "or_bounds", "("+or_discovery['lower']+"-"+or_discovery['upper']+")")
+                else:
+                    setattr(h, "or_bounds", "("+str(float("{0:.2f}".format(1/float(or_discovery['upper'])))) + "-" +
+                            str(float("{0:.2f}".format(1/float(or_discovery['lower']))))+")")
+
+        if getattr(h, "odds_ratio") is not None:
+            if float(getattr(h, "odds_ratio")) > 1:
+                setattr(h, "risk_allele", getattr(h, "alleles")['minor'])
+            else:
+                setattr(h, "odds_ratio", str(float("{0:.2f}".format(1/float(getattr(h, "odds_ratio"))))))
+                setattr(h, "risk_allele", getattr(h, "alleles")['major'])
+    return docs
+
+
+def _region_up_down(genes, regions_start, regions_stop):
+    ''' Separate into within region and upstream and downstream arrays.'''
+    region = []
+    up = []
+    down = []
+    for doc in genes:
+        this_start = getattr(doc, "start")
+        this_stop = getattr(doc, "stop")
+        if((this_start > regions_start and this_start < regions_stop) or
+           (this_stop > regions_start and this_stop < regions_stop) or
+           (this_start < regions_start and this_stop > regions_stop)):
+            region.append(doc)
+        elif this_start < regions_start:
+            down.append(doc)
+        else:
+            up.append(doc)
+    return (region, up, down)
 
 
 def get_genes_for_region(seqid, start, end, must=None):
