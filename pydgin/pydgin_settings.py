24dc02101902737c820e70101793775a6fddeae3
Premanand Achuthan <prem.apa@gmail.com>
2016-04-06 13:43:30 +0100

    tmp commit

--- pydgin/pydgin_settings.py
+++ pydgin/pydgin_settings.py
@@ -6,7 +6,7 @@ DEFAULT_BUILD = 38
 PAGE_SECTIONS = {
     'GeneView': OrderedDict([
         ('overview', {'show': True, 'collapse': False}),
-        ('igvBrowser', {'show': True, 'collapse': False}),
+        #('igvBrowser', {'show': True, 'collapse': False}),
         ('external links', True),
         ('criteria', True),
         ('study', True),
@@ -16,7 +16,7 @@ PAGE_SECTIONS = {
         ('phenotype links', True)]),
     'MarkerView': OrderedDict([
         ('overview', {'show': True, 'collapse': False}),
-        ('igvBrowser', {'show': True, 'collapse': False}),
+        #('igvBrowser', {'show': True, 'collapse': False}),
         ('historical ids', True),
         ('criteria', True),
         ('functional information', True),
@@ -32,7 +32,7 @@ PAGE_SECTIONS = {
     'RegionView': OrderedDict([
         ('overview', {'show': True, 'collapse': False}),
         ('criteria', True),
-        ('igvBrowser', {'show': True, 'collapse': False}),
+        #('igvBrowser', {'show': True, 'collapse': False}),
         ('study', True),
         ('publication', {'show': True})])
 }
