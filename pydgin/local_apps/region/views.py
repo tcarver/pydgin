814bb75204128d0116aea76569e3299d3d7051e4
tcarver <tim.carver@cimr.cam.ac.uk>
2016-04-14 10:49:50 +0100

    add comment

--- pydgin/local_apps/region/views.py
+++ pydgin/local_apps/region/views.py
@@ -128,7 +128,7 @@ class RegionTableView(TemplateView):
                 }
                 regions.append(region)
 
-        # IC stats
+        # look for pleiotropy by looking for diseases for the markers in IC_STATS and other study hits
         stats_query = ElasticQuery.filtered(Query.terms("marker", all_markers),
                                             Filter(RangeQuery("p_value", lte=5E-08)))
         stats_docs = Search(stats_query, idx=ElasticSettings.idx("IC_STATS"), size=len(all_markers)).search().docs
