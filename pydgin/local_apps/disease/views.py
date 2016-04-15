cbb09a2493d5ad2a0a2768908556fdac144e5aef
tcarver <tim.carver@cimr.cam.ac.uk>
2016-04-15 12:24:32 +0100

    add studies section

--- pydgin/local_apps/disease/views.py
+++ pydgin/local_apps/disease/views.py
@@ -11,6 +11,7 @@ from elastic.search import ElasticQuery, Search
 import gene
 from region.document import DiseaseLocusDocument
 from study.document import StudyDocument
+from core.document import PublicationDocument
 
 
 class DiseaseView(TemplateView):
@@ -86,7 +87,17 @@ class DiseaseView(TemplateView):
                                 diseases.append(doc.disease)
                     region['diseases'] = diseases
 
-                setattr(dis, 'studies',  StudyDocument.get_studies(disease_code=dis_code))
+                studies = StudyDocument.get_studies(disease_code=dis_code)
+                for doc in studies:
+                    setattr(doc, 'study_id', getattr(doc, 'study_id').replace('GDXHsS00', ''))
+                    pmid = getattr(doc, 'principal_paper')
+                    pubs = PublicationDocument.get_publications(pmid, sources=['date', 'authors.name', 'journal'])
+                    if len(pubs) > 0:
+                        authors = getattr(pubs[0], 'authors')
+                        setattr(doc, 'date', getattr(pubs[0], 'date'))
+                        setattr(doc, 'journal', getattr(pubs[0], 'journal'))
+                        setattr(doc, 'author', authors[0]['name'].rsplit(None, 1)[-1] if authors else "")
+                setattr(dis, 'studies',  studies)
 
             context['features'] = disease_docs
             context['title'] = names
