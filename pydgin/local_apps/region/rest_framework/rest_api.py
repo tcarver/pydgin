d62b223547c8fd9b2af9574d51263e593f4e2ee8
tcarver <tim.carver@cimr.cam.ac.uk>
2016-05-09 14:24:53 +0100

    add a GFF renderer

--- pydgin/local_apps/region/rest_framework/rest_api.py
+++ pydgin/local_apps/region/rest_framework/rest_api.py
@@ -1,7 +1,10 @@
 ''' Region DRF web-services. '''
-from rest_framework import serializers, mixins
+from rest_framework import serializers, mixins, renderers
 from region.rest_framework.region_resources import ListRegionsMixin
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+import csv
+from six import StringIO
 
 
 class GeneCategory(serializers.Serializer):
@@ -23,14 +26,53 @@ class DiseaseRegionSerializer(serializers.Serializer):
     genes = GeneCategory()
 
 
+class GFFRenderer(renderers.BaseRenderer):
+    ''' Render GFF format. '''
+    media_type = 'text/gff'
+    format = 'gff'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        csv_buffer = StringIO()
+        csv_writer = csv.writer(csv_buffer, delimiter="\t")
+        header = self.header(renderer_context['request'])
+        for hdr in header:
+            csv_writer.writerow([hdr])
+
+        for row in data:
+            csv_writer.writerow(self.row_data(row))
+        return csv_buffer.getvalue()
+
+    def row_data(self, row):
+        ''' GFF row data. '''
+        raise NotImplementedError("Inheriting class should implement this method")
+
+    def header(self, request):
+        ''' List of GFF header lines. '''
+        return ['##gff-version 3']
+
+
+class DiseaseRegionGFFRenderer(GFFRenderer):
+    ''' Render regions as in GFF format. '''
+    def row_data(self, row):
+        return [row['seqid'], 'immunobase', 'region', row['rstart'], row['rstart'],
+                '.', '.', '.',
+                'Name='+row['region_name']+';genes='+','.join(row['ens_cand_genes']) +
+                ';markers='+','.join(row['markers']) +
+                ';diseases='+','.join(row['all_diseases'])]
+
+    def header(self, request):
+        disease = request.GET.get('disease', 'T1D')
+        return ['##gff-version 3', '##'+disease+' Regions from immunobase.org']
+
+
 class DiseaseRegionViewSet(ListRegionsMixin, mixins.ListModelMixin, GenericViewSet):
-    ''' Given a feature (e.g. gene, marker, region) and the build return the location(s).
+    ''' Given a disease (e.g. gene, marker, region) and the build return the location(s).
     ---
     list:
         response_serializer: DiseaseRegionSerializer
         parameters:
-            - name: feature
-              description: gene, marker or region (e.g. IL2, rs2476601).
+            - name: disease
+              description: disease
               required: false
               type: string
               paramType: query
@@ -39,6 +81,10 @@ class DiseaseRegionViewSet(ListRegionsMixin, mixins.ListModelMixin, GenericViewS
               required: false
               type: string
               paramType: query
+        produces:
+            - application/json
+            - text/gff
     '''
+    renderer_classes = (JSONRenderer, DiseaseRegionGFFRenderer, BrowsableAPIRenderer, )
     serializer_class = DiseaseRegionSerializer
     filter_fields = ('disease', 'build')
